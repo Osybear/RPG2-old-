f0ef95a38b8ac64e3548be5a846898143811ebb0
Osybear <osybeargames@gmail.com>
2019-10-04 17:27:30 -0500

    Update

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,13 +7,31 @@ public class Health : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
-    
+
+    private GameObject healthBar;
+    private Image healthImage;
+    public GameObject healthPrefab;
+    public Transform holder;
+    public new Camera camera;
+    public Vector3 offset;
+
     private void Awake() {
-        
+        healthBar = Instantiate(healthPrefab);
+        healthBar.transform.SetParent(holder);
+        healthImage = healthBar.GetComponent<Image>();
     }
 
+    private void FixedUpdate() {
+        healthBar.transform.position = camera.WorldToScreenPoint(transform.position + offset);
+    }
+
+    public void OnDamage(int amount) {
+         curHealth = curHealth - amount;
+         SetHealthBar();
+    }
     
-    //create a health bar at the transform position on the screen
-    //and then move it up
-    //but that mean anythin that isnt the player needs it position updated
-}
+    public void SetHealthBar() {
+        float fillPercentage = (float)curHealth / maxHealth;
+        healthImage.fillAmount = fillPercentage;
+    }
+}   
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//https://gamedev.stackexchange.com/questions/118675/need-help-with-a-field-of-view-like-collision-detector
 public class PlayerController : MonoBehaviour
 {
     public new Rigidbody rigidbody;
     public new Camera camera;
     public int moveSpeed;
     public int lookSpeed;
+    public int damage;
+    public float detectionRadius;
+    public float maxAttackAngle;
 
     private void Awake() {
         rigidbody = GetComponent<Rigidbody>();
@@ -17,6 +20,8 @@ public class PlayerController : MonoBehaviour
     private void Update() {
         MovePlayer();
         RotatePlayer();
+        if(Input.GetMouseButtonDown(0))
+            AttackUnit();
     }
 
     public void RotatePlayer() {
@@ -43,4 +48,42 @@ public class PlayerController : MonoBehaviour
         rigidbody.velocity = currentVelocity;
     }
 
+    public GameObject GetNearestUnit() {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5);
+        float lastAngle = float.MaxValue;
+        GameObject lastCol = null;
+
+        foreach(Collider col in hitColliders){
+            if(!col.name.Contains("Enemy"))
+                continue;
+
+            float a = Vector3.Distance(transform.position + (transform.forward * detectionRadius), col.transform.position);
+            float b = detectionRadius;
+            float c = Vector3.Distance(transform.position, col.transform.position);
+            float angle = Mathf.Acos(((b * b) + (c * c) - (a * a)) / (2 * b * c)) * Mathf.Rad2Deg;
+            Debug.Log("LastAngle : " + angle);
+            if(angle < lastAngle) {
+                lastCol = col.gameObject;
+                lastAngle = angle;
+            }
+        }
+    
+        if(lastAngle < maxAttackAngle)
+            return lastCol;
+        return null;
+    }
+
+    public void AttackUnit() {
+        GameObject unit = GetNearestUnit();
+
+        if(unit == null)
+            return; 
+        
+        Health health = unit.GetComponent<Health>();
+
+        if(health == null)
+            return;
+
+        health.OnDamage(damage);
+    }
 }
