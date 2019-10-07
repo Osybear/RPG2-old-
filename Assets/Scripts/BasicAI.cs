8d94ae90ba5789d4c0bf6f3b08131654976490fa
Osybear <osybeargames@gmail.com>
2019-10-07 16:16:21 -0500

    Update

--- Assets/Scripts/BasicAI.cs
+++ Assets/Scripts/BasicAI.cs
@@ -16,54 +16,6 @@ public class BasicAI : MonoBehaviour
     private Coroutine attackRoutine;
 
     private void FixedUpdate() {
-        if(aggroPlayer != null && agent.remainingDistance < attackDistance) {
-            agent.isStopped = true;
-            isAttacking = true;
-        }
-
-        if(isAttacking == true && attackRoutine == null) {
-            attackRoutine = StartCoroutine(Attack());
-        }
-
-        if(aggroPlayer == null) {
-            aggroPlayer = GetNearestPlayer();
-        }
-
-        if(aggroPlayer != null) {
-            agent.SetDestination(aggroPlayer.transform.position);
-        }
-    }
-
-    public IEnumerator Attack() {
-        while(Vector3.Distance(transform.position, aggroPlayer.transform.position) < attackDistance) {
-            yield return new WaitForSeconds(attackSpeed);
-            Health health = aggroPlayer.GetComponent<Health>();
-            health.OnDamage(damage);
-        }
-
-        isAttacking = false;
-        attackRoutine = null;
-        agent.isStopped = false;
-        yield return null;
-    }
-
-    public Collider GetNearestPlayer() {
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius, overlapMask);
         
-        if(hitColliders.Length == 0)
-            return null;
-
-        if(hitColliders.Length == 1)
-            return hitColliders[0];
-
-        Collider nearestCollider = hitColliders[0];
-
-        for (int i = 1; i < hitColliders.Length; i++) {
-            if(Vector3.Distance(transform.position, hitColliders[i].transform.position) < Vector3.Distance(transform.position, nearestCollider.transform.position)) {
-                nearestCollider = hitColliders[i];
-            }
-        }
-
-        return nearestCollider;
     }
 }
